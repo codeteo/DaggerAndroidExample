05eee170c01a45d898a92080a70871dfafd0c9c8
codeteo <ramones.teo@gmail.com>
2018-02-11 00:50:02 +0200

    Adds dummy view for MainFragment

--- app/src/main/java/teo/example/com/features/main/MainFragment.java
+++ app/src/main/java/teo/example/com/features/main/MainFragment.java
@@ -1,8 +1,15 @@
 package teo.example.com.features.main;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 import javax.inject.Inject;
 
 import dagger.android.support.DaggerFragment;
+import teo.example.com.R;
 import teo.example.com.dagger.scopes.ActivityScope;
 
 /**
@@ -19,6 +26,12 @@ public class MainFragment extends DaggerFragment implements MainMVP.View {
     @Inject
     public MainFragment() {
         // Requires empty public constructor
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
     }
 
     @Override

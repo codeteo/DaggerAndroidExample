3ea7faa10bf3cabaa12efaae8d3919b3f59cb785
codeteo <ramones.teo@gmail.com>
2018-02-10 21:22:05 +0200

    Adds MyApplication class

---
+++ app/src/main/java/teo/example/com/MyApplication.java
@@ -0,0 +1,21 @@
+package teo.example.com;
+
+import android.app.Application;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+
+
+/**
+ * We create a custom {@link Application} class that extends  {@link DaggerApplication}.
+ * We then override applicationInjector() which tells Dagger how to make our @Singleton Component
+ * We never have to call `component.inject(this)` as {@link DaggerApplication} will do that for us.
+ */
+
+public class MyApplication extends DaggerApplication{
+
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return null;
+    }
+}
